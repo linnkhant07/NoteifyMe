0bb5a352de49c8b79859e406644e19d6bbc3ecde
thuya, linn khant <lkty07@gmail.com>
2023-04-22 09:19:11 -0700

    add models

---
+++ models/course.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+//strictQuery - only the specified fields in the Schema will be accepted
+//to remove depreciation
+mongoose.set('strictQuery', true);
+const Schema = mongoose.Schema;
+
+//DONT FORGET TO DO PRE AND POSTS!!!
+
+const Note = require('./note');
+const { boolean } = require('joi');
+
+const courseSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true
+    },
+
+    instructor: {
+      type: String,
+      required: true
+    },
+
+    notes: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Chapter'
+    }],
+
+    isRemind: boolean
+});
+
+//if course is deleted, delete the notes
+courseSchema.post('findOneAndDelete', async (course) =>{
+    //if there is any note
+    if(course.notes.length){
+        const res = await Note.deleteMany({_id: {$in: course.chapters}})
+    }
+})
+
+const Course = mongoose.model('Course', courseSchema)
+module.exports = Course;
